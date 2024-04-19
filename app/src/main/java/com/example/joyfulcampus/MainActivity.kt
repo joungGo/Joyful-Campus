f0013435fcbd29dd2d6a10ffc295cc68f6f4ca9a
joung-pjh <jounghyeon123@daum.net>
2024-04-19 18:31:09 +0900

    fix sidebar(complete-exept design)

--- app/src/main/java/com/example/joyfulcampus/MainActivity.kt
+++ app/src/main/java/com/example/joyfulcampus/MainActivity.kt
@@ -5,6 +5,8 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
@@ -35,24 +37,62 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         toolbarBodyTemplate.title = "편안한 캠퍼스"
 
+// NavigationView의 메뉴 텍스트 색상 설정
+        binding.mainSidebar.itemTextColor = ContextCompat.getColorStateList(this, R.color.black)
 
+        // NavigationView 아이템 클릭 리스너 설정
+        binding.mainSidebar.setNavigationItemSelectedListener { menuItem ->
+            // 메뉴 아이템 클릭 시 사이드바를 닫기
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            // 클릭 이벤트 처리
+            when (menuItem.itemId) {
+                R.id.menu1 -> {
+                    // TODO: 메뉴 아이템1에 대한 처리 작성
+                    true
+                }
+                R.id.menu2 -> {
+                    // TODO: 메뉴 아이템2에 대한 처리 작성
+                    true
+                }
+                R.id.menu3 -> {
+                    // TODO: 메뉴 아이템3에 대한 처리 작성
+                    true
+                }
+                R.id.menu4 -> {
+                    // TODO: 메뉴 아이템4에 대한 처리 작성
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-    // 챗봇 버튼 구현
+    // 챗봇 버튼 Click Event
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()) {
+        when (item.itemId) {
             android.R.id.home -> {
-                // TODO : 챗봇 icon 클릭 후 발생하는 이벤트 구현하기
+                // 홈 버튼 클릭 시 사이드바 열기
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+                return true
             }
             R.id.chat_bot -> {
-                // TODO : 챗봇 icon 클릭 후 발생하는 이벤트 구현하기
+                // 챗봇 아이템 클릭 시 동작 작성
+                return true
             }
+            else -> return super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
+    }
+    override fun onBackPressed() {
+        // 뒤로가기 버튼을 눌렀을 때 사이드바가 열려 있다면 닫기
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
