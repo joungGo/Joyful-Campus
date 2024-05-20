e0214268a37cf330b824fef2c18f19c53f42acc6
joung-pjh <jounghyeon123@daum.net>
2024-05-20 23:18:38 +0900

    apply design per icon..(~ing)

--- app/src/main/java/com/example/joyfulcampus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/joyfulcampus/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.joyfulcampus.R
 import com.example.joyfulcampus.databinding.FragmentHomeBinding
@@ -19,9 +20,11 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         val toolbarBodyTemplate = view.findViewById<Toolbar>(R.id.homeToolbar)
         (requireActivity() as AppCompatActivity).setSupportActionBar(toolbarBodyTemplate)
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true) // 홈 버튼 활성화
-        (requireActivity() as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_apps_24)
+        (requireActivity() as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.list)
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
         toolbarBodyTemplate.title = "편안한 캠퍼스"
+        toolbarBodyTemplate.setTitleTextColor(ContextCompat.getColor(requireContext(), android.R.color.black))
+
     }
 
 
